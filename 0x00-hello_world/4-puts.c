5725a433594e4326441abaff02deca1e75546600
olatunjhi <kinghorlatunjhi@gmail.com>
2023-06-15 19:02:21 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 
 int main(void) 
 { 
-        puts ("\"Programming is like building a multilingual puzzle");  /* print out string function */
-        return(0);
+puts ("\"Programming is like building a multilingual puzzle");  /* print out string function */
+return(0);
 }
