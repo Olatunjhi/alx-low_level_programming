f45c1712aef5d09e54e4b73ce53a15f1158c30e6
olatunjhi <kinghorlatunjhi@gmail.com>
2023-06-15 18:33:42 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-   #include <stdio.h>
+#include <stdio.h>
 
 
- int main(void) 
- { 
-       /* print out string function */
-       puts ("\"Programming is like building a multilingual puzzle");
-       return(0);
- }
+int main(void) 
+{ 
+      /* print out string function */
+      puts ("\"Programming is like building a multilingual puzzle");
+      return(0);
+}
