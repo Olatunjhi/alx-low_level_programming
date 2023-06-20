a271e3dec6d91043ace2b1ea9f7a0a95e0172f72
olatunjhi <kinghorlatunjhi@gmail.com>
2023-06-20 12:14:15 -0700

    create 1-alphabet program
    create 1-alphabet.c file
    
    update main.h && create 1-main.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main - To print alphabet in lowercase to stdo.
+ *
+ *Return: always success o.
+ */
+
+void print_alphabet(void)
+{
+
+int alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
+}
