6e1a990ac3c5c3715b7d105b3058215db072f82f
olatunjhi <kinghorlatunjhi@gmail.com>
2023-06-20 13:36:07 -0700

    update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,22 +4,22 @@
 /**
  *print_alphabet_x10: To print lowercase alphabet to stdo.
  *
+ *Return: always 0.
  */
 
- void print_alphabet_x10(void)
- {
+void print_alphabet_x10(void)
+{
 
- int alphabet_x10;
- int line_10;
+int alphabet_x10;
+int line_10;
 
- for (line_10 = 0; line_10 <= 10; line_10++)
- {
- for (alphabet_x10 = 'a'; alphabet_x10 <= 'z'; alphabet_x10++)
- {
- _putchar(alphabet_x10);
- }
- _putchar('\n');
- }
+for (line_10 = 0; line_10 < 10; line_10++)
+{
+for (alphabet_x10 = 'a'; alphabet_x10 <= 'z'; alphabet_x10++)
+{
+_putchar(alphabet_x10);
+}
+_putchar('\n');
+}
 
-
- }
+}
