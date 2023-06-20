f081c949111d0ea98a6166d84bcc7e80df7ea488
olatunjhi <kinghorlatunjhi@gmail.com>
2023-06-20 13:29:45 -0700

    create 2-print_alphabet_x10.c
    run 2-alphabet_x10
    
     create 2-main.c && update main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ *print_alphabet_x10: To print lowercase alphabet to stdo.
+ *
+ */
+
+ void print_alphabet_x10(void)
+ {
+
+ int alphabet_x10;
+ int line_10;
+
+ for (line_10 = 0; line_10 <= 10; line_10++)
+ {
+ for (alphabet_x10 = 'a'; alphabet_x10 <= 'z'; alphabet_x10++)
+ {
+ _putchar(alphabet_x10);
+ }
+ _putchar('\n');
+ }
+
+
+ }
