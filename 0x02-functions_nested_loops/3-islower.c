ea912410da4bce04eea25bdb99d07b763c149b0c
olatunjhi <kinghorlatunjhi@gmail.com>
2023-06-21 12:20:49 -0700

    update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,19 +3,21 @@
 
 /**
  *_islower - To check for lowercase alphabet.
+ *parameter(c) - It serve as ascii value.
  *
  *Return: always 0.
  */
 
- int _islower(int c)
- {
+int _islower(int c)
+{
 
- if (c > 97 && c < 122)
- {
- return (1);
- }
- else
- {
- return (0);
- }
- }
+if (c > 97 && c < 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
