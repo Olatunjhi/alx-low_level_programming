0415265629f82c93f62e8d31bb1d5c48bb233718
olatunjhi <kinghorlatunjhi@gmail.com>
2023-06-21 17:59:50 -0700

    create 6-abs && 6-abs.c
    create 6-main.c && update main.h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_abs - To print absolute value of int.
+ *@i: variable of int type.
+ *
+ *Return: Always 0, success.
+ */
+
+int _abs(int i)
+{
+
+if (i > 0)
+{
+return (1);
+}
+else if (i == 0)
+{
+return (0);
+}
+else
+{
+return (i * -1);
+}
+}
