592f344e91f65bbb882e3e24154b5f275f882b29
olatunjhi <kinghorlatunjhi@gmail.com>
2023-06-23 21:46:59 -0700

    create new files and update main.h

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - To print 0 to 9 and skip 4 and 5.
+ *
+ *Return: always 0.
+ */
+
+
+void print_most_numbers(void)
+{
+
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+
+		if (number == 2 || number == 4)
+		{
+			continue;
+		}
+
+		_putchar(number);
+	}
+
+	_putchar('\n');
+}
