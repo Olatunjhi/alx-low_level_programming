032162f5fe4e86f97f8495f0005c4864ce791f04
olatunjhi <kinghorlatunjhi@gmail.com>
2023-06-24 04:26:29 -0700

    create new files && update main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *more_numbers - To print 0 to 14 in 10 lines.
+ */
+
+
+void more_numbers(void)
+{
+
+	int i, j;
+	for (i = 0; i < 11; i++)
+	{
+	for (j = 0; j < 15; j++)
+	{
+	
+	if(j < 10)
+	{
+	_putchar(j + '0');
+	}
+	else
+	{
+	_putchar('1');
+	_putchar((j - 10) + '0');
+	}
+	}
+	_putchar('\n');
+	}
+}
