80b8c1ac246762fef0b77c24ea435bdca8f762ac
olatunjhi <kinghorlatunjhi@gmail.com>
2023-06-27 00:34:44 -0700

    create new files

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *swap_int - To swap the value of a to b.
+ *@*a: integer pointer a.
+ *@*b: integer pointer b.
+ *@a: integer a.
+ *@b: integer b.
+ *
+ */
+
+
+void swap_int(int *a, int *b)
+{
+
+	*a = 42;
+	*b = 98;
+}
