12b02e91590d3819735582c9877e8e8c64e1b359
olatunjhi <kinghorlatunjhi@gmail.com>
2023-09-28 17:43:56 -0700

    To print exponent of numbers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _pow_recursion - Function to print exponent of number
+ * @y: secomd argument in prototype
+ * @x: first argument in prototype
+ * Return: -1, 1 & function result
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return(1);
+	}
+	else
+	{
+		return(x * _pow_recursion(x, y-1));
+	}
+
+}
