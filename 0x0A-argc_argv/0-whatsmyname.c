4ac6e3db6fdd52ac0b705238edc78e5bbc596434
olatunjhi <kinghorlatunjhi@gmail.com>
2023-10-02 11:50:50 -0700

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - main function .
+ * @argc: argument count
+ * @*argv[]: argument velocity.
+ * Return: Always 0
+ *
+ */
+
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
