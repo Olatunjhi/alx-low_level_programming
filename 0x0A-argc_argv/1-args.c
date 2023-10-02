fa464826b1cc643b094ff6bf57a4665ce227b924
olatunjhi <kinghorlatunjhi@gmail.com>
2023-10-02 16:21:04 -0700

    updated

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Funtion that count for command line argument
+ * @argc: argument count
+ * @argv: argument velocity or array
+ *
+ * return: Always 0
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
