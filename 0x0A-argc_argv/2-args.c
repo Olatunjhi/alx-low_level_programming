94b7f52a5e65d74995a4893fc4dd216d7a4a199c
olatunjhi <kinghorlatunjhi@gmail.com>
2023-10-02 17:02:28 -0700

    updated

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints all arguments its receive
+ * @argc: argument count
+ * @argv: argument velocity
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
