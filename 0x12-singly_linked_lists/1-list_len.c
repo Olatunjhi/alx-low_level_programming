bce57bfe0c133a19f444a6da8a05f530ef02d72a
olatunjhi <kinghorlatunjhi@gmail.com>
2023-10-19 19:07:25 -0700

    updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,9 +10,13 @@ size_t list_len(const list_t *h)
 {
 	int i;
 
-	for (i = 0; h->next != NULL; i++)
+	if (h == NULL)
 	{
-		i++;
+		return (0);
+	}
+
+	for (i = 1; h->next != NULL; i++)
+	{
 		h = h->next;
 	}
 
