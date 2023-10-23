439068656698a8b9d0db8d992c2e9eef04eafe63
olatunjhi <kinghorlatunjhi@gmail.com>
2023-10-23 15:27:36 -0700

    updated

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+	listint_t *nextNode;
+
+	temp = head;
+
+	while (temp != NULL)
+	{
+		nextNode = temp->next;
+		free(temp);
+		
+		temp = nextNode;
+	}
+}
