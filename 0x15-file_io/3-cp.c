08c722325f0e2b4f8505eb4443a46698ea7e6b64
olatunjhi <kinghorlatunjhi@gmail.com>
2023-10-30 16:15:29 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,70 @@
 #include "main.h"
 
 /**
+ * read_file - Reads the content of a file and returns the read data
+ * @filename: The name of the file to read
+ * @buffer: A buffer to store the read data
+ * @buffer_size: The size of the buffer
+ * Return: The number of bytes read on success, -1 on failure
+ */
+ssize_t read_file(const char *filename, char *buffer, size_t buffer_size)
+{
+	ssize_t num_read;
+
+	int source_fd = open(filename, O_RDONLY);
+
+	if (source_fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", filename);
+		return (-1);
+	}
+
+	num_read = read(source_fd, buffer, buffer_size);
+
+	close(source_fd);
+
+	if (num_read == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", filename);
+		return (-1);
+	}
+
+	return (num_read);
+}
+
+/**
+ * write_file - Writes data to a file
+ * @filename: The name of the file to write to
+ * @data: The data to write
+ * @data_size: The size of the data
+ * Return: 0 on success, otherwise an error code
+ */
+int write_file(const char *filename, const char *data, size_t data_size)
+{
+	ssize_t num_written;
+
+	int dest_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (dest_fd == -1)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", filename);
+		return (99);
+	}
+
+	num_written = write(dest_fd, data, data_size);
+
+	close(dest_fd);
+
+	if (num_written != (ssize_t)data_size)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", filename);
+		return (99);
+	}
+
+	return (0);
+}
+
+/**
  * main - main function
  * @ac: command line
  * @av: command line
